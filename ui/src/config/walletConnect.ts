bcf4585a2e9645568d695017fc00f99373303be6
devarogundade <devarogundade@gmail.com>
2025-09-23 15:12:16 +0100

    https://mavuno.netlify.app

--- ui/src/config/walletConnect.ts
+++ ui/src/config/walletConnect.ts
@@ -2,6 +2,6 @@ export const metadata = {
   name: "Mavuno",
   description:
     "Empowering rural farmers through community-backed micro-lending on Hedera Hashgraph.",
-  url: "https://mavuno.netlify.app",
+  url: "https://mavuno-hedera.netlify.app",
   icons: ["https://avatars.githubusercontent.com/u/31002956"],
 };
